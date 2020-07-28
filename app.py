3438d10103ca63078dcb03915ebdf66ba6f241b1
Shivam Sharma <shivam231198@gmail.com>
2020-07-28 10:25:46 +0530

    bug fix

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ def Get_All_Cases():
     
     for case in client.db.cases.find():
         data = {
-            name: case['name'],
-            caseID: case['_id']
+            "name": case['name'],
+            "caseID": case['_id']
         }
         res.append(data)
     return jsonify({"cases": res}), 200
@@ -91,7 +91,7 @@ def Get_A_Case():
     if (not case):
         return jsonify({"error": "invalid case ID"})
 
-    return jsonify(case), 200
+    return jsonify({case}), 200
 
 
 # To select a bag inorder to Set a Target
